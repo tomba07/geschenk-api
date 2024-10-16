e57716036cc2e344d1d70cd26f243ee00049c44c
Mirko Teschke <mirko.teschke@sap.com>
2024-10-16 10:04:43 -0700

    Add Project Assigned Flag

---
+++ prisma/migrations/20241016170423_add_assigned_flag/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "assigned" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Project" ("id", "name") SELECT "id", "name" FROM "Project";
+DROP TABLE "Project";
+ALTER TABLE "new_Project" RENAME TO "Project";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
