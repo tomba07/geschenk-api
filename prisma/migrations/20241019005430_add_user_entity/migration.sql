149d26ac9fe3e958ddf41aff7114c9bb3a539b5f
Mirko Teschke <mirko.teschke@gmail.com>
2024-10-18 17:55:55 -0700

    Add User Entity

---
+++ prisma/migrations/20241019005430_add_user_entity/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Project` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
